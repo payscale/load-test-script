949beb28a74e36583b154e45ff977aa51f8427aa
Taras Mitran <taras.mitran@payscale.com>
2019-08-23 13:46:33 -0400

    various improvements

--- load-test.js
+++ load-test.js
@@ -17,16 +17,16 @@ const convertFileToArrayOfData = (file) => {
   console.log(`Reading ./data/${file}`);
   const fileContents = fs.readFileSync(`./data/${file}`, { encoding: 'utf8' });
 
-  var rows = fileContents.split('\n');
-  for (var i = 0; i < rows.length; i++) {
+  let rows = fileContents.split('\n');
+  for (let i = 0; i < rows.length; i++) {
     col = rows[i].split('\t');
     rows[i] = col;
   }
-  var rows = rows.slice(1); // Delete the first row - it is just '#Version: 1.0'
-  var headers = rows[0][0];
+  rows = rows.slice(1); // Delete the first row - it is just '#Version: 1.0'
+  let headers = rows[0][0];
   headers = headers.split('\ ') // Split the headers row by space, it isn't tab separated
   headers = headers.slice(1); // Delete the first header, it is just 'Fields'
-  var dto = {}
+  let dto = {}
   headers.forEach(h => {
     dto[h] = null;
   });
@@ -35,7 +35,7 @@ const convertFileToArrayOfData = (file) => {
     rows = rows.filter(row => { return row[7] ? row[7].startsWith(pathPrefix) : false});
   }
   
-  var arr = [];
+  let arr = [];
   rows.forEach((m, i) => {
     arr[i] = JSON.parse(JSON.stringify(dto)); // clone the DTO
     Object.keys(dto).forEach((prop, j) => {
@@ -47,13 +47,15 @@ const convertFileToArrayOfData = (file) => {
 }
 
 /** Ping all the urls with the correct timeouts */
+console.time('totalPingTime');
+const initialDate = new Date();
 const pingUrls = (rows) => {
   if (maxPings) {
     rows = rows.slice(0, maxPings);
   }
 
-  var i = 0;
-  var numTimes = rows.length - 1;
+  let i = 0;
+  const numTimes = rows.length - 1;
   const doTheThing = () => {
     const timeBeforeNextPing = new Date(`1970-01-01T${rows[i+1].time}Z`).getTime() - new Date(`1970-01-01T${rows[i].time}Z`).getTime();
     ping(rows[i]['cs-uri-stem'], rows[i].time);
@@ -61,22 +63,39 @@ const pingUrls = (rows) => {
     i++;
     if (i < numTimes) {
       if (timeBeforeNextPing > 0) {
-        console.log(`timeout for ${timeBeforeNextPing}ms`)
+        const durationSec = (new Date() - initialDate) / 1000;
+        const reqPerSec = Math.round(i / durationSec * 100) / 100;
+        console.log(`timeout for ${timeBeforeNextPing}ms after ${i} requests 
+                     in ${durationSec} seconds for ${reqPerSec} req/sec`);
       }
       setTimeout(doTheThing, timeBeforeNextPing);
-    }
+    } else {
+      const totalPingTime = console.timeEnd('totalPingTime');
+      const totalDurationSec = (new Date() - initialDate) / 1000;
+      const totalReqPerSec = Math.round(i / totalDurationSec * 100) / 100;
+      console.log(`run complete for ${numTimes} requests in time ${totalPingTime}ms`, 
+                  ` for ${totalReqPerSec} req/sec`,
+                  'summary stats of request counts by statusCode:', statusCodeCounts);
+    }  
   }
 
   doTheThing();
 }
 
 /** The ping function */
+let statusCodeCounts = {};
+let poolSettings = { maxSockets: 20 };
+let responseCount = 0;
 const ping = (urlStem, time) => {
-  var separateReqPool = {maxSockets: 20};
 
-  request(`${urlHost}${urlStem}`, {time: true, pool: separateReqPool}, (err, res, body) => {
+  request(`${urlHost}${urlStem}`, {time: true, pool: poolSettings}, (err, res, body) => {
     if (err) { return console.log(err); }
-    console.log(res.statusCode === 200 ? '\x1b[32m%s\x1b[0m' : res.statusCode === 404 ? "\x1b[33m%s\x1b[0m" : '\x1b[31m%s\x1b[0m', `${time}: Took ${res.elapsedTime}ms; ${urlStem} returned ${res.statusCode}.`);
+    responseCount++;
+    console.log(res.statusCode === 200 ? '\x1b[32m%s\x1b[0m' : res.statusCode === 404 ? "\x1b[33m%s\x1b[0m" : '\x1b[31m%s\x1b[0m', `${time}: Took ${res.elapsedTime}ms; ${urlStem} returned ${res.statusCode}. Count: ${responseCount}`);
+    if (!statusCodeCounts[res.statusCode]) {
+      statusCodeCounts[res.statusCode] = 0;
+    }
+    statusCodeCounts[res.statusCode]++;
   });
 }
 
